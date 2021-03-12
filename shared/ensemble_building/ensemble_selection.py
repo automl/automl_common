a9ff5858221735b7b3737b37bbe66abe1b405506
chico <francisco.rivera.valverde@gmail.com>
2021-03-12 15:13:58 +0100

    Initial Ensemble Selection Push

---
+++ shared/ensemble_building/ensemble_selection.py
@@ -0,0 +1,305 @@
+import random
+from collections import Counter
+from typing import Callable, List, Tuple, Union
+
+import numpy as np
+
+from sklearn.pipeline import Pipeline
+
+from shared.ensemble_building.abstract_ensemble import AbstractEnsemble
+
+
+class EnsembleSelection(AbstractEnsemble):
+    """Implementation of Ensemble Selection from the paper:
+    Ensemble selection from libraries of models, Caruana 2016.
+
+    This class builds a set of weights that are used to aggregate
+    individual scikit-learn pipelines's prediction.
+
+    Attributes
+    ----------
+    ensemble_size : int
+        The maximum number of individual members that an ensemble can have.
+
+    calculate_loss: Callable
+        A function with signature calculate_loss(solutions, predictions) that estimate
+        the loss of the current predictions vs the expected solutions. The current
+        implementation of ensemble selections solves a minimization problem.
+    random_state: np.random.RandomState
+        A random number generator used to break ties when 2 models provide the same
+        performance gain.
+    bagging: bool
+        Enables the bagging approach from Ensemble selection from libraries
+        of models, Caruana 2016.
+    mode: str ['fast', 'slow']
+        Selects between the fast and slow (but intuitive) implementation of ensemble
+        selection
+
+    Returns
+    -------
+    self
+    """
+    def __init__(
+        self,
+        ensemble_size: int,
+        calculate_loss: Callable,
+        random_state: np.random.RandomState,
+        bagging: bool = False,
+        mode: str = 'fast',
+    ) -> None:
+        self.ensemble_size = ensemble_size
+        self.calculate_loss = calculate_loss
+        self.bagging = bagging
+        self.mode = mode
+        self.random_state = random_state
+
+    def fit(
+        self,
+        predictions: List[np.ndarray],
+        labels: np.ndarray,
+        identifiers: List[Tuple[int, int, float]],
+    ) -> AbstractEnsemble:
+        self.ensemble_size = int(self.ensemble_size)
+        if self.ensemble_size < 1:
+            raise ValueError('Ensemble size cannot be less than one!')
+        if self.mode not in ('fast', 'slow'):
+            raise ValueError('Unknown mode %s' % self.mode)
+
+        if self.bagging:
+            self._bagging(predictions, labels)
+        else:
+            self._fit(predictions, labels)
+        self._calculate_weights()
+        self.identifiers_ = identifiers
+        return self
+
+    def _fit(
+        self,
+        predictions: List[np.ndarray],
+        labels: np.ndarray,
+    ) -> AbstractEnsemble:
+        if self.mode == 'fast':
+            self._fast(predictions, labels)
+        else:
+            self._slow(predictions, labels)
+        return self
+
+    def _fast(
+        self,
+        predictions: List[np.ndarray],
+        labels: np.ndarray,
+    ) -> None:
+        """Fast version of Rich Caruana's ensemble selection method."""
+        self.num_input_models_ = len(predictions)
+
+        ensemble = []  # type: List[np.ndarray]
+        trajectory = []
+        order = []
+
+        ensemble_size = self.ensemble_size
+
+        weighted_ensemble_prediction = np.zeros(
+            predictions[0].shape,
+            dtype=np.float64,
+        )
+        fant_ensemble_prediction = np.zeros(
+            weighted_ensemble_prediction.shape,
+            dtype=np.float64,
+        )
+        for i in range(ensemble_size):
+            losses = np.zeros(
+                (len(predictions)),
+                dtype=np.float64,
+            )
+            s = len(ensemble)
+            if s > 0:
+                np.add(
+                    weighted_ensemble_prediction,
+                    ensemble[-1],
+                    out=weighted_ensemble_prediction,
+                )
+
+            # Memory-efficient averaging!
+            for j, pred in enumerate(predictions):
+                # fant_ensemble_prediction is the prediction of the current ensemble
+                # and should be ([predictions[selected_prev_iterations] + predictions[j])/(s+1)
+                # We overwrite the contents of fant_ensemble_prediction
+                # directly with weighted_ensemble_prediction + new_prediction and then scale for avg
+                np.add(
+                    weighted_ensemble_prediction,
+                    pred,
+                    out=fant_ensemble_prediction
+                )
+                np.multiply(
+                    fant_ensemble_prediction,
+                    (1. / float(s + 1)),
+                    out=fant_ensemble_prediction
+                )
+
+                # calculate_loss is versatile and can return a dict of losses
+                # when scoring_functions=None, we know it will be a float
+                losses[j] = self.calculate_loss(labels, fant_ensemble_prediction)
+
+            all_best = np.argwhere(losses == np.nanmin(losses)).flatten()
+            best = self.random_state.choice(all_best)
+            ensemble.append(predictions[best])
+            trajectory.append(losses[best])
+            order.append(best)
+
+            # Handle special case
+            if len(predictions) == 1:
+                break
+
+        self.indices_ = order
+        self.trajectory_ = trajectory
+        self.train_loss_ = trajectory[-1]
+
+    def _slow(
+        self,
+        predictions: List[np.ndarray],
+        labels: np.ndarray
+    ) -> None:
+        """Rich Caruana's ensemble selection method."""
+        self.num_input_models_ = len(predictions)
+
+        ensemble = []
+        trajectory = []
+        order = []
+
+        ensemble_size = self.ensemble_size
+
+        for i in range(ensemble_size):
+            losses = np.zeros(
+                [np.shape(predictions)[0]],
+                dtype=np.float64,
+            )
+            for j, pred in enumerate(predictions):
+                ensemble.append(pred)
+                ensemble_prediction = np.mean(np.array(ensemble), axis=0)
+                # calculate_loss is versatile and can return a dict of losses
+                # when scoring_functions=None, we know it will be a float
+                losses[j] = self.calculate_loss(labels, ensemble_prediction)
+                ensemble.pop()
+            best = np.nanargmin(losses)
+            ensemble.append(predictions[best])
+            trajectory.append(losses[best])
+            order.append(best)
+
+            # Handle special case
+            if len(predictions) == 1:
+                break
+
+        self.indices_ = np.array(
+            order,
+            dtype=np.int64,
+        )
+        self.trajectory_ = np.array(
+            trajectory,
+            dtype=np.float64,
+        )
+        self.train_loss_ = trajectory[-1]
+
+    def _calculate_weights(self) -> None:
+        ensemble_members = Counter(self.indices_).most_common()
+        weights = np.zeros(
+            (self.num_input_models_,),
+            dtype=np.float64,
+        )
+        for ensemble_member in ensemble_members:
+            weight = float(ensemble_member[1]) / self.ensemble_size
+            weights[ensemble_member[0]] = weight
+
+        if np.sum(weights) < 1:
+            weights = weights / np.sum(weights)
+
+        self.weights_ = weights
+
+    def _bagging(
+        self,
+        predictions: List[np.ndarray],
+        labels: np.ndarray,
+        fraction: float = 0.5,
+        n_bags: int = 20,
+    ) -> np.ndarray:
+        """Rich Caruana's ensemble selection method with bagging."""
+        raise ValueError('Bagging might not work with class-based interface!')
+        n_models = predictions.shape[0]
+        bag_size = int(n_models * fraction)
+
+        order_of_each_bag = []
+        for j in range(n_bags):
+            # Bagging a set of models
+            indices = sorted(random.sample(range(0, n_models), bag_size))
+            bag = predictions[indices, :, :]
+            order, _ = self._fit(bag, labels)
+            order_of_each_bag.append(order)
+
+        return np.array(
+            order_of_each_bag,
+            dtype=np.int64,
+        )
+
+    def predict(self, predictions: Union[np.ndarray, List[np.ndarray]]) -> np.ndarray:
+
+        average = np.zeros_like(predictions[0], dtype=np.float64)
+        tmp_predictions = np.empty_like(predictions[0], dtype=np.float64)
+
+        # if predictions.shape[0] == len(self.weights_),
+        # predictions include those of zero-weight models.
+        if len(predictions) == len(self.weights_):
+            for pred, weight in zip(predictions, self.weights_):
+                np.multiply(pred, weight, out=tmp_predictions)
+                np.add(average, tmp_predictions, out=average)
+
+        # if prediction model.shape[0] == len(non_null_weights),
+        # predictions do not include those of zero-weight models.
+        elif len(predictions) == np.count_nonzero(self.weights_):
+            non_null_weights = [w for w in self.weights_ if w > 0]
+            for pred, weight in zip(predictions, non_null_weights):
+                np.multiply(pred, weight, out=tmp_predictions)
+                np.add(average, tmp_predictions, out=average)
+
+        # If none of the above applies, then something must have gone wrong.
+        else:
+            raise ValueError("The dimensions of ensemble predictions"
+                             " and ensemble weights do not match!")
+        del tmp_predictions
+        return average
+
+    def __str__(self) -> str:
+        return 'Ensemble Selection:\n\tTrajectory: %s\n\tMembers: %s' \
+               '\n\tWeights: %s\n\tIdentifiers: %s' % \
+               (' '.join(['%d: %5f' % (idx, performance)
+                         for idx, performance in enumerate(self.trajectory_)]),
+                self.indices_, self.weights_,
+                ' '.join([str(identifier) for idx, identifier in
+                          enumerate(self.identifiers_)
+                          if self.weights_[idx] > 0]))
+
+    def get_models_with_weights(
+        self,
+        models: Pipeline
+    ) -> List[Tuple[float, Pipeline]]:
+        output = []
+        for i, weight in enumerate(self.weights_):
+            if weight > 0.0:
+                identifier = self.identifiers_[i]
+                model = models[identifier]
+                output.append((weight, model))
+
+        output.sort(reverse=True, key=lambda t: t[0])
+
+        return output
+
+    def get_selected_model_identifiers(self) -> List[Tuple[int, int, float]]:
+        output = []
+
+        for i, weight in enumerate(self.weights_):
+            identifier = self.identifiers_[i]
+            if weight > 0.0:
+                output.append(identifier)
+
+        return output
+
+    def get_validation_performance(self) -> float:
+        return self.trajectory_[-1]
