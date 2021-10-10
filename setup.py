56ef78724a70283048c7be90d652924590afa76b
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2021-10-10 12:01:10 +0200

    [fix] Fix based on the black formatter

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 requirements = []
-with open('requirements.txt', 'r') as f:
+with open("requirements.txt", "r") as f:
     for line in f:
         requirements.append(line.strip())
 
@@ -17,12 +17,12 @@ setuptools.setup(
     description=("Shared utilities that AutoML frameworks may benefit from."),
     long_description=long_description,
     url="https://github.com/automl/automl_common",
-    license='Apache License 2.0',
+    license="Apache License 2.0",
     keywords="machine learning algorithm configuration hyperparameter "
-             "optimization tuning neural architecture deep learning",
+    "optimization tuning neural architecture deep learning",
     packages=setuptools.find_packages(),
-	python_requires='>=3.7',
-    platforms=['Linux'],
+    python_requires=">=3.7",
+    platforms=["Linux"],
     install_requires=requirements,
-    include_package_data=True
+    include_package_data=True,
 )
