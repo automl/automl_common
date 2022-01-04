4504f4601b99ed193989d047bfffbb82afec625e
eddiebergman <eddiebergmanhs@gmail.com>
2022-01-04 22:52:38 +0100

    Update: workflow related files

--- setup.py
+++ setup.py
@@ -1,40 +1,46 @@
 import setuptools
 
-
 with open("README.md", "r") as f:
     long_description = f.read()
 
-requirements = []
 with open("requirements.txt", "r") as f:
-    for line in f:
-        requirements.append(line.strip())
+    requirements = [line.strip() for line in f]
 
-extra_reqs = {
-    "test": {
-        "pytest",
-        "flake8",
-        "flake8-import-order",
-        "mypy",
-        "pytest-timeout",
-        "pre-commit",
-        "pytest-cov",
-    }
-}
 
 setuptools.setup(
     name="automl_common",
     version="0.0.1",
     author="AutoML Freiburg",
     author_email="feurerm@informatik.uni-freiburg.de",
-    description=("Shared utilities that AutoML frameworks may benefit from."),
+    description="Shared utilities that AutoML frameworks may benefit from.",
     long_description=long_description,
     url="https://github.com/automl/automl_common",
     license="Apache License 2.0",
-    keywords="machine learning algorithm configuration hyperparameter "
-    "optimization tuning neural architecture deep learning",
+    keywords=" ".join(
+        [
+            "machine learning",
+            "algorithm configuration",
+            "hyperparameter optimization",
+            "tuning",
+            "neural architecture",
+            "deep learning",
+        ]
+    ),
     packages=setuptools.find_packages(),
     python_requires=">=3.7",
     platforms=["Linux"],
     install_requires=requirements,
+    extra_reqs={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "pydocstyle",
+            "flake8",
+            "black",
+            "isort",
+            "mypy",
+            "pre-commit",
+        ]
+    },
     include_package_data=True,
 )
