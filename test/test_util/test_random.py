171f4d0ad24669b93c3ba99eda05376e84163040
eddiebergman <eddiebergmanhs@gmail.com>
2022-01-11 02:01:47 +0100

    Add: test for failure case of `as_random_state`

--- test/test_util/test_random.py
+++ test/test_util/test_random.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 import numpy as np
+import pytest
 from pytest_cases import parametrize
 
 from automl_common.util.random import as_random_state
@@ -20,3 +21,19 @@ def test_as_random_state(seed: Union[int, np.random.RandomState, None]) -> None:
     """
     rng = as_random_state(seed)
     assert isinstance(rng, np.random.RandomState)
+
+
+@parametrize("bad_seed", [object(), 0.1])
+def test_as_random_state_bad_seed(bad_seed: Union[object, float]) -> None:
+    """
+    Parameters
+    ----------
+    bad_seed: Union[object, float]
+        A seed that won't convert to RandomState
+
+    Expects
+    -------
+    * Should return a random state object
+    """
+    with pytest.raises(ValueError):
+        as_random_state(bad_seed)
