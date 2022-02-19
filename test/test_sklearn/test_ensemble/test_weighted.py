d612e661c0a9e766952243b3957a94f91185962b
eddiebergman <eddiebergmanhs@gmail.com>
2022-02-19 21:41:24 +0100

    Add: Tests for weighted sklearn ensemble

--- test/test_sklearn/test_ensemble/test_weighted.py
+++ test/test_sklearn/test_ensemble/test_weighted.py
@@ -1,22 +1,87 @@
 import pytest
+from pytest_cases import filters as ft
+from pytest_cases import parametrize_with_cases
+
+from sklearn.exceptions import NotFittedError
+
+from automl_common.sklearn.ensemble import WeightedEnsemble
+
+from test.test_sklearn.test_ensemble.cases import cases
 
 
-@pytest.mark.todo
-def test_trajectory_when_fitted() -> None:
+@parametrize_with_cases("ensemble", cases=cases, has_tag=["fitted", "weighted"])
+def test_trajectory(ensemble: WeightedEnsemble) -> None:
     """
+    Parameters
+    ----------
+    ensemble: WeightedEnsemble
+        A fitted weighted ensemble
 
     Expects
     -------
     * Should be able to get tracjectory_ without issue
+    * Trajectory length should be same as it's size
+    * The trajectory should be sorted either min -> max or max -> min
     """
-    pass
+    traj = ensemble.tracjectory
+
+    assert len(traj) == ensemble.size
+
+    scores = [score for _, score in traj]
+    assert scores == sorted(scores) or scores == sorted(scores, reverse=True)
 
 
-@pytest.mark.todo
-def test_trajectory_when_not_fitted() -> None:
+@parametrize_with_cases("ensemble", cases=cases, has_tag=["fitted", "weighted"])
+def test_weights(ensemble: WeightedEnsemble) -> None:
+    """
+    Parameters
+    ----------
+    ensemble : WeightedEnsemble
+        A fitted ensemble
+
+    Expects
+    -------
+    * The weights should add up to 1
+    * Each model in weight should exist in the ensemble
+    """
+    assert sum(ensemble.weights.values()) == pytest.approx(1)
+    assert all(model_name in ensemble for model_name in ensemble.weights)
+
+
+@parametrize_with_cases(
+    "ensemble",
+    cases=cases,
+    filter=ft.has_tag("weighted") & ~ft.has_tag("fitted"),
+)
+def test_trajectory_when_not_fitted(ensemble: WeightedEnsemble) -> None:
     """
     Expects
     -------
     * Should raise a NotFittedError
     """
-    pass
+    with pytest.raises(NotFittedError):
+        ensemble.tracjectory
+
+    return  # pragma: no cover
+
+
+@parametrize_with_cases(
+    "ensemble",
+    cases=cases,
+    filter=ft.has_tag("weighted") & ~ft.has_tag("fitted"),
+)
+def test_weights_when_not_fitted(ensemble: WeightedEnsemble) -> None:
+    """
+    Parameters
+    ----------
+    ensemble : WeightedEnsemble
+        A non-fitted ensemble
+
+    Expects
+    -------
+    * Accessing weights should raise NotFittedError when ensemble is not fitted
+    """
+    with pytest.raises(NotFittedError):
+        ensemble.weights
+
+    return  # pragma: no cover
