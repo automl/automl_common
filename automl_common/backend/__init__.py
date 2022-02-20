d7fc45d2651196a69ea06b361b361e22f4d29b73
eddiebergman <eddiebergmanhs@gmail.com>
2022-02-21 00:57:12 +0100

    Add: Easier imports from backend

--- automl_common/backend/__init__.py
+++ automl_common/backend/__init__.py
@@ -1,3 +1,22 @@
+from automl_common.backend.accessors.ensemble_accessor import EnsembleAccessor
+from automl_common.backend.accessors.model_accessor import ModelAccessor
 from automl_common.backend.backend import Backend
+from automl_common.backend.stores.ensemble_store import EnsembleStore
+from automl_common.backend.stores.model_store import ModelStore
+from automl_common.backend.stores.numpy_store import NumpyStore
+from automl_common.backend.stores.pickle_store import PickleStore
+from automl_common.backend.stores.predictions_store import PredictionsStore
+from automl_common.backend.stores.store import Store, StoreView
 
-__all__ = ["Backend"]
+__all__ = [
+    "Backend",
+    "EnsembleStore",
+    "ModelStore",
+    "NumpyStore",
+    "PickleStore",
+    "PredictionsStore",
+    "Store",
+    "StoreView",
+    "EnsembleAccessor",
+    "ModelAccessor",
+]
